7469808335ca0f3476afda8f1924b05817a62970
Fakhar Anwar <fakharanwar@hotmail.com>
2022-10-12 15:51:07 +0100

    fix(TaskTemplate): Avoid Running Task Template in Testing Environment

--- database/migrations/2022_10_10_110240_update_task_template_from_default.php
+++ database/migrations/2022_10_10_110240_update_task_template_from_default.php
@@ -10,6 +10,10 @@ return new class extends Migration {
      */
     public function up()
     {
+        if (app()->runningUnitTests()) {
+            return;
+        }
+
         copy(
             storage_path('valuestore/quote.task.template.json'),
             storage_path(sprintf("valuestore/quote.task.template.json.bak.%s", now()->format('YmdHis')))
