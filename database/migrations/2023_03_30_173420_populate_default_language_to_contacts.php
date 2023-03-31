d7b169d5ca7c001613ded082b6b4adafd4a0bd75
Fakhar Anwar <fakharanwar@hotmail.com>
2023-03-31 18:56:12 +0100

    fix(database): Populated Default Contact Language Migration

--- database/migrations/2023_03_30_173420_populate_default_language_to_contacts.php
+++ database/migrations/2023_03_30_173420_populate_default_language_to_contacts.php
@@ -1,16 +1,35 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 return new class() extends Migration {
     public function up(): void
     {
-        Artisan::call('db:seed', [
-            '--force' => true,
-            '--class' => \Database\Seeders\LanguageSeeder::class,
-        ]);
+        $seeds = yaml_parse_file(database_path('seeders/models/languages.yaml'));
+
+        $seeds = collect($seeds)
+            ->lazy()
+            ->map(static function (array $seed): array {
+                return [
+                    'id' => Str::orderedUuid()->toString(),
+                    'code' => $seed['code'],
+                    'name' => $seed['name'],
+                    'native_name' => $seed['nativeName'],
+                ];
+            })
+            ->collect();
+
+        DB::transaction(static function () use ($seeds): void {
+            foreach ($seeds as $seed) {
+                DB::table('languages')
+                    ->upsert($seed, 'code', [
+                        'name' => $seed['name'],
+                        'native_name' => $seed['native_name'],
+                    ]);
+            }
+        });
 
         $lang = DB::table('languages')
             ->where('code', 'en')
