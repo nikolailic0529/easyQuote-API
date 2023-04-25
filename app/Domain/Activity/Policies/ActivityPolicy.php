fed6ebdfcfeea89cda3caecad05b025431765faa
Fakhar Anwar <fakharanwar@hotmail.com>
2023-04-25 17:29:46 +0100

    chore(Audit): Updated Responses in the Policy

--- app/Domain/Activity/Policies/ActivityPolicy.php
+++ app/Domain/Activity/Policies/ActivityPolicy.php
@@ -4,7 +4,9 @@ namespace App\Domain\Activity\Policies;
 
 use App\Domain\Activity\Models\Activity;
 use App\Domain\User\Models\User;
+use App\Foundation\Auth\Access\Response\ResponseBuilder;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class ActivityPolicy
 {
@@ -12,21 +14,39 @@ class ActivityPolicy
 
     /**
      * Determine whether the user can view any activities.
-     *
-     * @return mixed
      */
-    public function viewAny(User $user)
+    public function viewAny(User $user): Response
     {
-        return $user->can('view_activities');
+        if ($user->hasRole(R_SUPER)) {
+            return $this->allow();
+        }
+
+        if ($user->can('view_activities')) {
+            return $this->allow();
+        }
+
+        return ResponseBuilder::deny()
+            ->action('view')
+            ->item('audit')
+            ->toResponse();
     }
 
     /**
      * Determine whether the user can view the activity.
-     *
-     * @return mixed
      */
-    public function view(User $user, Activity $activity)
+    public function view(User $user, Activity $activity): Response
     {
-        return $user->can('view_activities');
+        if ($user->hasRole(R_SUPER)) {
+            return $this->allow();
+        }
+
+        if ($user->can('view_activities')) {
+            return $this->allow();
+        }
+
+        return ResponseBuilder::deny()
+            ->action('view')
+            ->item('audit')
+            ->toResponse();
     }
 }
