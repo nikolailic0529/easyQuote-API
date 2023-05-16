ef1665ee93a4dad8963bbc05b24fc2b038c74556
Fakhar Anwar <fakharanwar@hotmail.com>
2023-05-16 15:27:47 +0100

    fix(SalesOrder): Mapped & Validated Invoice Address Upon SO Submission
    * Explicitly Mapped & Validated Invoice Address Upon Sales Order Submission

--- app/Domain/Worldwide/DataTransferObjects/SalesOrder/Submit/SubmitSalesOrderData.php
+++ app/Domain/Worldwide/DataTransferObjects/SalesOrder/Submit/SubmitSalesOrderData.php
@@ -7,90 +7,66 @@ use Symfony\Component\Validator\Constraints;
 
 final class SubmitSalesOrderData extends DataTransferObject
 {
-    /**
-     * @Constraints\NotBlank(message="At least one address must be present.")
-     * @Constraints\All(@Constraints\Type("\App\Domain\Worldwide\DataTransferObjects\SalesOrder\Submit\SubmitOrderAddressData"))
-     *
-     * @var \App\Domain\Worldwide\DataTransferObjects\SalesOrder\Submit\SubmitOrderAddressData[]
-     */
-    public array $addresses_data;
+    #[Constraints\NotNull(message: 'Invoice address is missing.')]
+    public ?SubmitOrderAddressData $invoice_address;
+
+    public ?SubmitOrderAddressData $hardware_address;
 
     public SubmitOrderCustomerData $customer_data;
 
     /**
-     * @Constraints\NotBlank(message="At least one order line must be present.")
-     * @Constraints\All(@Constraints\Type("\App\Domain\Worldwide\DataTransferObjects\SalesOrder\Submit\SubmitOrderLineData"))
-     *
      * @var \App\Domain\Worldwide\DataTransferObjects\SalesOrder\Submit\SubmitOrderLineData[]
      */
+    #[Constraints\NotBlank(message: 'At least one order line must be present.')]
+    #[Constraints\All([new Constraints\Type(SubmitOrderLineData::class)])]
     public array $order_lines_data;
 
     public string $vendor_short_code;
 
-    /**
-     * @Constraints\Choice({"Pack", "Contract"}, message="Contract type must be either [Pack] or [Contract].")
-     */
+    #[Constraints\Choice(['Pack', 'Contract'], message: 'Contract type must be either [Pack] or [Contract].')]
     public string $contract_type;
 
     public ?string $registration_customer_name;
 
-    /**
-     * @Constraints\Currency
-     */
+    #[Constraints\Currency]
     public string $currency_code;
 
-    /**
-     * @Constraints\Date
-     */
+    #[Constraints\Date]
     public ?string $from_date;
 
-    /**
-     * @Constraints\Date
-     */
+    #[Constraints\Date]
     public ?string $to_date;
 
     public ?string $service_agreement_id;
 
-    /**
-     * @Constraints\NotBlank
-     */
+    #[Constraints\NotBlank]
     public string $order_no;
 
-    /**
-     * @Constraints\Date
-     */
+    #[Constraints\Date]
     public string $order_date;
 
-    /**
-     * @Constraints\Choice({
-     *     "SWH",
-     *     "EPD",
-     *     "EPD%20IT%20Services%20LLC",
-     *     "EPD%20Asia%20Pacific%20Pty%20Ltd",
-     *     "TESFR",
-     *     "TESDE",
-     *     "TESAT",
-     *     "TESTS",
-     *     "TESTES"
-     * })
-     */
+    #[Constraints\Choice([
+        'SWH',
+        'EPD',
+        'EPD%20IT%20Services%20LLC',
+        'EPD%20Asia%20Pacific%20Pty%20Ltd',
+        'TESFR',
+        'TESDE',
+        'TESAT',
+        'TESTS',
+        'TESTES',
+    ])]
     public string $bc_company_name;
 
-    /**
-     * @Constraints\Uuid
-     */
+    #[Constraints\Uuid]
     public string $company_id;
 
     public string $sales_person_name;
 
-    /**
-     * @Constraints\NotNull(message="Exchange rate is missing.")
-     */
+    #[Constraints\NotNull(message: 'Exchange rate is missing.')]
     public ?float $exchange_rate;
 
-    /**
-     * @Constraints\Uuid
-     */
+    #[Constraints\Uuid]
     public string $post_sales_id;
 
     public string $customer_po;
